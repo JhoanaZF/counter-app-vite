80bc560ca55eadc69b89d03578ceb6539ace4349
Jhoana Zamora <jhoanazamora2@gmail.com>
2023-08-25 01:04:27 -0500

    ADD: Colocar estilos de CSS

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -3,14 +3,14 @@
 //     title: "Jhoana",
 // };
 
-const myFunction = () => {
-    return 4 + 4;
-};
+// const myFunction = () => {
+//     return 4 + 4;
+// };
 
 export const FirstApp = () => {
     return (
         <>
-            <h1>{myFunction()}</h1>
+            {/* <h1>{myFunction()}</h1> */}
             {/* <code>{JSON.stringify(newMessage)}</code> */}
             <h1>Jhoana</h1>
             <p>Soy un subtitulo</p>
--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 // import { HelloWorldApp } from "./HelloWorldApp";
 import { FirstApp } from "./FirstApp";
-
 // import App from "./HelloWorldApp";
+import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
