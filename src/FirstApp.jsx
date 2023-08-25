db8acdac8ec2afde17897a34234b47d540def76b
Jhoana Zamora <jhoanazamora2@gmail.com>
2023-08-25 00:54:09 -0500

    ADD: Impresión de variables en el HTML

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -1,6 +1,17 @@
+// const newMessage = {
+//     message: "Hola Mundo",
+//     title: "Jhoana",
+// };
+
+const myFunction = () => {
+    return 4 + 4;
+};
+
 export const FirstApp = () => {
     return (
         <>
+            <h1>{myFunction()}</h1>
+            {/* <code>{JSON.stringify(newMessage)}</code> */}
             <h1>Jhoana</h1>
             <p>Soy un subtitulo</p>
         </>
