b58db2a87786a5677bded2199b3937f9db4516b7
Jhoana Zamora <jhoanazamora2@gmail.com>
2023-08-29 00:39:14 -0500

    ADD: DefaultProps

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -7,17 +7,23 @@
 //     return 4 + 4;
 // };
 import PropTypes from "prop-types";
-export const FirstApp = ({ title, subTitle }) => {
+export const FirstApp = ({ title, subTitle, name }) => {
     return (
         <>
             {/* <h1>{myFunction()}</h1> */}
             {/* <code>{JSON.stringify(newMessage)}</code> */}
             <h1>{title}</h1>
             <p>{subTitle}</p>
+            <p>{name}</p>
         </>
     );
 };
 FirstApp.propTypes = {
     title: PropTypes.string.isRequired,
-    subTitle: PropTypes.number.isRequired,
+    subTitle: PropTypes.string,
+};
+FirstApp.defaultProps = {
+    title: "No hay titulo",
+    subTitle: "No hay subtitulo",
+    name: "Jhoana Zamora",
 };
