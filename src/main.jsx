21c91974003274c415c612fc06cc4cf5f1956cb4
Jhoana Zamora <jhoanazamora2@gmail.com>
2023-08-29 11:36:03 -0500

    ADD: Tarea - Componente CounterApp

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 // import { HelloWorldApp } from "./HelloWorldApp";
-import { FirstApp } from "./FirstApp";
+// import { FirstApp } from "./FirstApp";
 // import App from "./HelloWorldApp";
+import { CounterApp } from "./CounterApp";
 import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
         {/* <HelloWorldApp /> */}
-        <FirstApp />
+        {/* <FirstApp /> */}
+        <CounterApp value={23} />
     </React.StrictMode>
 );
